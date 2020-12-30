77bd159299f9ad4dc9be1e2911680ed58db0d817
StarlkYT <45287860+StarlkYT@users.noreply.github.com>
2020-12-31 00:04:11 +0300

    Update Block.gd

--- core/scripts/utils/Block.gd
+++ core/scripts/utils/Block.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var terrain_raycast: RayCast2D = $StaticBody2D/RayCast2D
 onready var terrain_sprite: Sprite = $StaticBody2D/Sprite
+onready var visibility_enabler: VisibilityEnabler2D = $VisibilityEnabler2D
+
 var dirt_sprite: Texture = load("res://assets/tiles/dirt_sprite.png")
 var grass_sprite: Texture = load("res://assets/tiles/grass_sprite.png")
 
@@ -10,7 +12,13 @@ func _process(_delta: float) -> void:
 		if terrain_raycast.is_colliding():
 			if not terrain_raycast.get_collider() is KinematicBody2D:
 				terrain_sprite.texture = dirt_sprite
-				terrain_raycast.queue_free()
+#				terrain_raycast.queue_free()
 		else:
-			terrain_raycast.queue_free()
+#			terrain_raycast.queue_free()
 			terrain_sprite.texture = grass_sprite
+
+func _on_VisibilityEnabler2D_screen_entered() -> void:
+	self.show()
+
+func _on_VisibilityEnabler2D_screen_exited() -> void:
+	self.hide()
