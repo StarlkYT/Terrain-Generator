f9067618d78e04685fd71af26e4ba934bc37484b
StarlkYT <45287860+StarlkYT@users.noreply.github.com>
2020-12-31 14:34:28 +0300

    ability to generate chunks in both direction

--- core/scripts/TerrainGenerator.gd
+++ core/scripts/TerrainGenerator.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-# TODO: Make terrain generates in both directions
-
 var random: RandomNumberGenerator = RandomNumberGenerator.new()
 var block_scene: PackedScene = load("res://core/scenes/utils/Block.tscn")
-var distance_position_x: int
+var distance_position_right: int = 96
+var distance_position_left: int = 32
+var generate_direction: int = 1 # 0 = right, 1 = left
 var body_node: Node
 
 onready var player_raycast_right: RayCast2D = $Player/Right
+onready var player_raycast_left: RayCast2D = $Player/Left
 onready var cursor: Node2D = $Cursor
 
 func _on_Area2D2_body_entered(body: Node) -> void:
@@ -28,6 +29,11 @@ func _process(_delta: float) -> void:
 	cursor.position.y = get_global_mouse_position().y
 	if not player_raycast_right == null:
 		if (player_raycast_right.enabled) and not (player_raycast_right.is_colliding()):
+			generate_direction = 0
+			generate_chunk()
+	if not player_raycast_left == null:
+		if (player_raycast_left.enabled) and not (player_raycast_left.is_colliding()):
+			generate_direction = 1
 			generate_chunk()
 
 func place_block(position_x: int, position_y: int) -> void:
@@ -37,8 +43,15 @@ func place_block(position_x: int, position_y: int) -> void:
 	block_instance.global_position.y = position_y
 
 func generate_chunk() -> void:
-	for _bar in range(4):
-		var random_position_y: int = random.randi_range(352, 480)
-		for _depth in range(8):
-			place_block(distance_position_x, (random_position_y + 64 * _depth))
-		distance_position_x += 64
+	if generate_direction == 0: # right
+		for _bar in range(4):
+			var random_position_y: int = random.randi_range(352, 480)
+			for _depth in range(8):
+				place_block(distance_position_right, (random_position_y + 64 * _depth))
+			distance_position_right += 64
+	else: # left
+		for _bar in range(4):
+			var random_position_y: int = random.randi_range(352, 480)
+			for _depth in range(8):
+				place_block(distance_position_left, (random_position_y + 64 * _depth))
+			distance_position_left -= 64
