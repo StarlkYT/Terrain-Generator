bb9095df4d43de739730b88479072b21337ed722
StarlkYT <45287860+StarlkYT@users.noreply.github.com>
2020-12-31 14:35:29 +0300

    remove unecessary print statement

--- core/scripts/utils/FPSMonitor.gd
+++ core/scripts/utils/FPSMonitor.gd
@@ -2,7 +2,6 @@ extends Label
 
 func _process(_delta: float) -> void:
 	var _FPS: int = int(Engine.get_frames_per_second())
-	print(OS.get_dynamic_memory_usage())
 	self.text = "FPS: " + str(_FPS)
 	if _FPS > 45:
 		self.set("custom_colors/font_color", Color8(0, 240, 0 , 255))
